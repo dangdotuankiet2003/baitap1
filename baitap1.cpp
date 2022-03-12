a88166276ca53539e5b50722f6b0395bf715a086
dangdotuankiet2003 <101450681+dangdotuankiet2003@users.noreply.github.com>
2022-03-12 15:36:07 +0700

    Add files via upload

---
+++ baitap1.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+main()
+{
+    cout<<"Ten: "<<"Dang Do Tuan Kiet"<<endl;
+    cout<<"MSSV: "<<"106210036"<<endl;
+    cout<<"Lop: "<<"21DTCLC1"<<endl;
+    return 0;
+}
